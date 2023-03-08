b7aba1e391d8dcf932e7e9b20ffc565ffb795881
folksnforks <folksnforks13@gmail.com>
2023-03-08 01:43:53 -0800

    1) handleSubmit function now creates/appends a <ul> to the DOM

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const submitBtn = document.getElementById('submit');
 const listSection = document.getElementById('list-section');
-let itemList;
+const itemList = document.createElement('ul'); // creates unordered list (not yet appended)
+itemList.setAttribute('id', 'user-item-list');
 
-function handleSubmit(event) {
-   event.preventDefault(event);
-
-   if (listSection) {
+function handleSubmit(event) { // when submit button is clicked (Mario assumes)
+   event.preventDefault(event); // prevent app from reloading
+   const eachItem = document.createElement('li');
+   eachItem.setAttribute('id', 'each-item');
+   
+   if (listDoesntExist) { //
       // add item to the existing list
+      listSection.appendChild(itemList); // makes the list (<ul>) (happens once)
+      
    } else {
       // list created here
+      // append to the dom
+      // and add item
    }
+   listSection.appendChild(eachItem); // appends each item
+   itemList.appendChild('each-item');
 }
 
-itemList = document.createElement('li');
-// itemList.setAttribute('id', 'user-items');
-itemList.id = 'user-items'; // Mario says: would this work?
-console.log('itemList', itemList)
+submitBtn.addEventListener('click', handleSumbit);
+// itemList.id = 'user-items'; // Mario says: would this work?
+// console.log('itemList', itemList);
 
-// It'sa me! Mario!
+/*
+  <div id='listsection'>
+    <ul id='user-item-list'>
+      <li> appended item after clicking submit </li>
+    </ul>
+  </div>
+*/
