afe44c18852440219735e6731ec6ac0aa155171f
ken-can-code <gxcadr@gmail.com>
2023-03-14 14:55:26 -0700

    Ken first commit added feature to append user input text to DOM

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const userItem = document.getElementById('user-item');
 const submitBtn = document.getElementById('submit');
-console.log('submitBtn', submitBtn);
 const listDiv = document.getElementById('list-div');
 const itemList = document.createElement('ul'); // creates unordered list (not yet appended)
 itemList.setAttribute('id', 'user-item-list');
 let idNum = 0;
 
 function handleSubmit(event) { // when submit button is clicked (Mario assumes)
-   event.preventDefault(event); // prevent app from reloading
-   const eachItem = document.createElement('li');
-   eachItem.setAttribute('id', `each-item${idNum}`);
-   idNum += 1;
-   
-   if (listDoesntExist) { // runs only once
-      listDiv.appendChild(itemList); // makes the list (<ul>) (happens once)
-   }
-   itemList.appendChild(eachItem);
+  event.preventDefault(event); // prevent app from reloading
+  const userInputText = userItem.value;
+  const eachItem = document.createElement('li');
+  eachItem.setAttribute('id', `each-item${idNum}`);
+  eachItem.textContent = userInputText;
+  idNum += 1;
+
+  if (itemList.isConnected === false) { // itemList is connected to a DOM
+    listDiv.appendChild(itemList); // makes the list (<ul>) (happens once)
+  }
+  itemList.appendChild(eachItem);
 }
 
 submitBtn.addEventListener('click', handleSubmit);
