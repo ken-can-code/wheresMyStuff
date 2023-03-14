0b5db78676622d22fc0621bc03f470c88d117e94
ken-can-code <gxcadr@gmail.com>
2023-03-14 15:55:43 -0700

    added branch and completed delete button feature

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const itemList = document.createElement('ul'); // creates unordered list (not ye
 itemList.setAttribute('id', 'user-item-list');
 let idNum = 0;
 
-function handleSubmit(event) { // when submit button is clicked (Mario assumes)
-  event.preventDefault(event); // prevent app from reloading
+function handleDelete(event) {
+  event.preventDefault(event);
+  event.target.parentNode.remove();
+}
+
+function handleSubmit(event) { // when submit button is clicked, enter works too
+  event.preventDefault(); // prevent app from reloading
   const userInputText = userItem.value;
+  userItem.value = ''; // clears input box after submitting
   const eachItem = document.createElement('li');
   eachItem.setAttribute('id', `each-item${idNum}`);
   eachItem.textContent = userInputText;
+  const deleteButton = document.createElement('button'); // create delete button in memory
+  deleteButton.textContent = 'delete';
+  eachItem.appendChild(deleteButton); // delete btn added on of right every item
+
+  deleteButton.addEventListener('click', handleDelete);
   idNum += 1;
 
   if (itemList.isConnected === false) { // itemList is connected to a DOM
@@ -20,13 +31,3 @@ function handleSubmit(event) { // when submit button is clicked (Mario assumes)
 }
 
 submitBtn.addEventListener('click', handleSubmit);
-
-// console.log('itemList', itemList);
-
-/*
-  <div id='listDiv'>
-    <ul id='user-item-list'>
-      <li> appended item after clicking submit </li>
-    </ul>
-  </div>
-*/
